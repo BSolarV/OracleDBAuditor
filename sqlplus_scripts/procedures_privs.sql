8512628c6d0a00ba6c984cefb0734211f5c9140c
Bastian Solar <bastian.solar.v@gmail.com>
2024-04-15 15:40:13 -0400

    updated command

--- sqlplus_scripts/procedures_privs.sql
+++ sqlplus_scripts/procedures_privs.sql
@@ -3,6 +3,6 @@ set linesize 500 pagesize 1000
 
 connect $USERNAME/$PASS@$HOST:$PORT/$SID
 
-SELECT * FROM dba_tab_privs WHERE table_name='UTL_FILE' OR table_name='UTL_HTTP' OR table_name='DBMS_JAVA' OR table_name='DBMS_JOB' OR table_name='DBMS_SCHEDULER' OR table_name='DBMS_ADVISOR' OR table_name='DBMS_XSLPROCESSOR' OR table_name='DBMS_LOB';
+SELECT * FROM dba_tab_privs WHERE table_name='UTL_FILE' OR table_name='UTL_HTTP' OR table_name='UTL_SMTP' OR table_name='UTL_TCP' OR table_name='DBMS_JAVA' OR table_name='DBMS_JOB' OR table_name='DBMS_SQL'OR table_name='DBMS_SCHEDULER' OR table_name='DBMS_ADVISOR' OR table_name='DBMS_XSLPROCESSOR' OR table_name='DBMS_LOB' OR table_name='DBMS_OBFUSCATION_TOOLKIT' OR table_name='OWA_UTIL';
 
 quit
