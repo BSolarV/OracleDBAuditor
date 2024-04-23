81183128415a7abdc6113b94726551a6460f671b
Bastián Solar Vargas <bastian.solar.v@gmail.com>
2024-04-23 17:01:58 -0400

    splitted scripts

--- sqlplus_scripts/pass_policy.sql
+++ sqlplus_scripts/pass_policy.sql
@@ -5,26 +5,4 @@ connect $USERNAME/$PASS@$HOST:$PORT/$SID
 
 SELECT resource_name,limit from dba_profiles where profile='DEFAULT';
 
-prompt "=========================="
-prompt Password verify function
-DECLARE
-  v_verify_function VARCHAR2(100);
-  v_ddl_query VARCHAR2(200);
-BEGIN
-  -- Recuperar el valor de PASSWORD_VERIFY_FUNCTION
-  SELECT PASSWORD_VERIFY_FUNCTION INTO v_verify_function
-  FROM dba_profiles
-  WHERE profile = 'DEFAULT'
-  AND resource_name = 'PASSWORD_VERIFY_FUNCTION';
-
-  -- Verificar si el valor no es NULL y ejecutar la consulta adicional
-  IF v_verify_function IS NOT NULL THEN
-	v_ddl_query := 'select dbms_metadata.get_ddl(''FUNCTION'',''' || v_verify_function || ''') from dual;';
-	EXECUTE IMMEDIATE v_ddl_query;
-  ELSE
-	DBMS_OUTPUT.PUT_LINE('No password verify found.');
-  END IF;
-END;
-/
-
 quit
--- sqlplus_scripts/version.sql
+++ sqlplus_scripts/version.sql
@@ -3,12 +3,6 @@ set linesize 500 pagesize 1000
 
 connect $USERNAME/$PASS@$HOST:$PORT/$SID
 
-
-prompt Version;
 SELECT * FROM V$VERSION; 
-
-prompt "=========================="
-prompt Installed Patches;
-SELECT action_time, action, namespace, version, comments FROM dba_registry_history;
 
 quit
